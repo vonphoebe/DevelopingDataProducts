72153ea79c37825e623baf63107022c9856073f5
vonphoebe <loucie_ji2005@hotmail.com>
2017-04-21 12:17:17 -0700

    Update server.R

--- server.R
+++ server.R
@@ -1,28 +1,61 @@
 
-library(shiny)
-mydata <- read.csv("~/NYSBRFSS2015c.csv", header=T)
-mytable <- table(mydata$GENHLTH1, mydata$BPHIGH4)
-#loghlth <- glm(GENHLTH1 ~ BPHIGH4,family=binomial,data=mydata)
+library(DT)
+
+mydat <- read.csv("kc_house_data.csv", header=T)
+mydata <- mydat[, c(2,3, 4,5, 6,7,15, 17)]
+mydata$SoldDate <- substr(mydata$date, 1, 8)
+mydata$SoldDate <- as.Date(mydata$SoldDate,
+                           format = "%Y%m%d")
+mydata <- mydata[, -1]
+mydata <- mydata[order(mydata$zipcode, mydata$bedrooms, mydata$yr_built), ]
 
 shinyServer(
-        function(input, output) {
+function(input, output) {
+        data <- mydata
+        
+        #observeEvent(input$goButton, NULL)
+        
+        df <- eventReactive(input$goButton, 
+                {(if (input$price != "All") {
+                        data <- data[mydata$price %in%
+                                            input$price[1]:input$price[2], ]
+                })        
+                 (if (input$zip != "All") {
+                        data <- data[data$zipcode == input$zip,]
+                        })
                 
-                output$out1 <- renderText(input$box1) 
-                output$out2 <- renderText(input$box2)
+                ( if (input$yearbt !="All") {
+                        data <- data[data$yr_built == input$yearbt,]
+                }) 
                 
+                (if (input$bedrooms != "All") {
+                                data <- data[data$bedrooms == input$bedrooms,]
+                                })                
                 
-                output$outplot1 <- renderPlot({
-                        plot(mydata$GENHLTH1)})
-                output$outplot2 <- renderPlot({
-                        plot(mydata$BPHIGH4)})
-                output$value <- renderTable({
-                        mytable 
-                     }) 
-                #output$value2 <- renderText({summary(loghlth)})
-                })
+                        }
+                )
+        
+        output$table <- DT::renderDataTable(
+                DT::datatable(df(),
+ # sorted columns are colored now because CSS are attached to them
+                        options =list(orderClasses = TRUE, autoWidth = TRUE), 
+                        filter = 'top')  
+                        )
+        
+        meanKC <- round(mean(mydata$price), digits=0)
+        medianKC <- round(median(mydata$price), digits=0 )
+        lowKC <- min(mydata$price)
+        highKC <- max(mydata$price)
+        
+        output$out2 <- renderText(paste0('The mean price of sold houses in King County from May 2014 to May 2015 was ', meanKC,
+                                         ' dollars, and the median price was ', medianKC, ' dollars, the price range was ', 
+                                         lowKC, ' dollars to ', highKC, ' dollars.')
+        )
+}
+)
 
 
- 
+
 
 
                 
