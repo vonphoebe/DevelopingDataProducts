f1b44ab49af5254f0de845b5e9f29e535f531ce7
vonphoebe <loucie_ji2005@hotmail.com>
2017-04-21 12:18:18 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,26 +1,44 @@
+
 library(shiny)
 
+mydat <- read.csv("kc_house_data.csv", header=T)
+mydata <- mydat[, c(2,3, 4,5, 6,7,15, 17)]
+mydata$SoldDate <- substr(mydata$date, 1, 8)
+mydata$SoldDate <- as.Date(mydata$SoldDate,
+                           format = "%Y%m%d")
+mydata <- mydata[, -1]
+
+mydata <- mydata[order(mydata$zipcode, mydata$bedrooms, mydata$yr_built), ]
+
 shinyUI(fluidPage(
-        titlePanel("General Health and High Blood Pressure in NYS 2015 BRFSS"), 
+        titlePanel("Basic DataTable of House Sales in King County, USA"),
         
-        sidebarLayout(
-        sidebarPanel(
-                selectInput("mydata", label=h4("Tab2: Would you say that in general your health is:"), 
-                            choices = levels(mydata$GENHLTH1 )), 
+        # Create a new Row in the UI for selectInputs
+        sidebarLayout(sidebarPanel(
+              "User Documentation: Use this App to help you get a price hint of your new home in the Great Seattle Area. Nallow down your choices based on the house price indicators.", 
+                selectInput("zip", "Zipcode:", 
+                            c("All", unique(as.character(mydata$zipcode)))), 
                 
-                selectInput("mydata", label=h4("Tab1: Ever told blood pressure high:"),
-                            choices =levels(mydata$BPHIGH4 )),
+                selectInput("bedrooms", "Number of Bedrooms:",
+                            c("All", unique(as.character(mydata$bedrooms)))), 
                 
-                h5("Tab3: Contigency table of 'General health' and 'Blood pressure'")),
+                selectInput("yearbt", "Year built:",
+                            c("All", unique(as.character(mydata$yr_built)))),
                 
-                mainPanel(tabsetPanel(type="tabs",
-                                      tabPanel("Tab1", br(), plotOutput('outplot1')),
-                                      tabPanel("Tab2", br(), plotOutput("outplot2")),
-                                      tabPanel("Tab3", br(), tableOutput("value"))
-                                      #tabPanel("Tab4", br(), textOutput("value2"))
-                                      
-                )
+                
+              sliderInput("price", label = h3("Price Range:"), min(mydata$price), 
+                           max(mydata$price), value = c(300000, 500000)),
+               
+               actionButton("goButton", "Go!"),
+               p("Click the button to update the datatable")
+        ),
+        # Create a new row for the table.
+        mainPanel(
+                tabsetPanel(type="tabs",
+                            tabPanel("Data", br(), 
+                DT::dataTableOutput("table")  ),
+                tabPanel("Summary", br(), textOutput("out2"))
                 )
         )
-
+)
 ))
